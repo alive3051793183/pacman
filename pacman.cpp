e26c003985fc649780da73997ad177ec5d1219ba
alive3051793183 <144124369+alive3051793183@users.noreply.github.com>
2024-05-09 18:18:35 +0800

    Create pacman.cpp

---
+++ pacman.cpp
@@ -0,0 +1,29 @@
+// pacman.cpp : 定义应用程序的入口点 。
+//
+
+#include "stdafx.h"
+#include "pacman.h"
+#include "GMap.h"
+#include "GObject.h"
+
+// 全局变量:
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+
+
+
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance,
+                      _In_ LPWSTR    lpCmdLine,
+                      _In_ int       nCmdShow)
+{
+    // 参数不再使用了
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
