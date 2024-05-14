0f628241735e09c1a3d90082e4f8b91370b5e953
alive3051793183 <144124369+alive3051793183@users.noreply.github.com>
2024-05-15 02:44:42 +0800

    Update pacman.cpp
    添加了bgm、倒计时，使之更有压迫感

--- pacman.cpp
+++ pacman.cpp
@@ -1,10 +1,15 @@
-// pacman.cpp : 定义应用程序的入口点 。
+/ pacman.cpp : 定义应用程序的入口点 。
 //
 
 #include "stdafx.h"
 #include "pacman.h"
 #include "GMap.h"
 #include "GObject.h"
+
+#include "windows.h"
+#include "mmsystem.h"
+#pragma comment(lib, "WINMM.LIB")
+
 
 using namespace std;
 
@@ -21,8 +26,6 @@ HINSTANCE hInst;                                // 当前实例
 WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
 WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
 
-
-
 // 此代码模块中包含的函数的前向声明:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
@@ -30,9 +33,9 @@ LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                      _In_opt_ HINSTANCE hPrevInstance,
-                      _In_ LPWSTR    lpCmdLine,
-                      _In_ int       nCmdShow)
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
 {
     // 参数不再使用了
     UNREFERENCED_PARAMETER(hPrevInstance);
@@ -44,7 +47,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     MyRegisterClass(hInstance);
 
     // 执行应用程序初始化:
-    if(!InitInstance(hInstance, nCmdShow)) {
+    if (!InitInstance(hInstance, nCmdShow)) {
         return FALSE;
     }
 
@@ -53,7 +56,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // 当前的关卡
     int s_n = 0; // [0, 1, 2]
     // 地图
-    GMap *MapArray[STAGE_COUNT] = { new Stage_1(), new Stage_2(), new Stage_3() };
+    GMap* MapArray[STAGE_COUNT] = { new Stage_1(), new Stage_2(), new Stage_3() };
 
     // 玩家对象
     // 自己
@@ -75,10 +78,16 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     DWORD dwLastTime = 0;
     // 主消息循环:
     // 玩家没有被抓,并且关卡<3
-    while(!g_me->IsOver() && s_n < STAGE_COUNT) {
+    while (!g_me->IsOver() && s_n < STAGE_COUNT) {
         // 判断是否赢得比赛
-        if(g_me->IsWin()) {
+        if (g_me->IsWin()) {
             s_n++;
+
+            if (s_n == 1)
+                PlaySound(LPWSTR(IDR_WAVE5), GetModuleHandle(NULL), SND_RESOURCE | SND_ASYNC | SND_LOOP);
+            if (s_n == 2)
+                PlaySound(LPWSTR(IDR_WAVE4), GetModuleHandle(NULL), SND_RESOURCE | SND_ASYNC | SND_LOOP);
+
             // 重设自己和敌人位置
             g_me->SetPosition(P_ROW, P_ARRAY);
             e1->SetPosition(E_ROW, E_ARRAY);
@@ -86,7 +95,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
             e3->SetPosition(E_ROW, E_ARRAY);
             e4->SetPosition(E_ROW, E_ARRAY);
             // 判断是否完成了3关,如果完成,退出游戏,否则进入下一关
-            if(s_n < 3) {
+            if (s_n < 3) {
                 MessageBox(g_hwnd, _T("恭喜过关"), _T("吃豆子提示"), MB_OK);
                 GObject::pStage = MapArray[s_n];
                 RECT screenRect;
@@ -98,7 +107,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                 HDC hdc = GetDC(g_hwnd);
                 std::shared_ptr<HDC__> dc(hdc, [](HDC hdc) {
                     ::ReleaseDC(g_hwnd, hdc);
-                });
+                    });
                 ::FillRect(dc.get(), &screenRect, CreateSolidBrush(RGB(255, 255, 255)));
                 GObject::pStage->DrawMap(hdc);
                 // 继续进行循环
@@ -111,14 +120,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         }
 
         // 获取消息
-        if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
 
-
         // 判断时间,否则画得太快
-        if(GetTickCount() - dwLastTime >= 40) {
+        if (GetTickCount() - dwLastTime >= 40) {
             dwLastTime = GetTickCount();
         }
         else {
@@ -132,7 +140,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
             // 不使用时自动释放
             std::shared_ptr<HDC__> dc(hdc, [](auto hdc) {
                 ::ReleaseDC(g_hwnd, hdc);
-            });
+                });
             // 画豆子
             MapArray[s_n]->DrawPeas(hdc);
             // 画地图
@@ -165,16 +173,16 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                 g_me->action();
 
                 // 获取按键 : 控制自己的方向
-                if(GetAsyncKeyState(VK_DOWN) & 0x8000) {
+                if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
                     g_me->SetTwCommand(DOWN);
                 }
-                if(GetAsyncKeyState(VK_LEFT) & 0x8000) {
+                if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
                     g_me->SetTwCommand(LEFT);
                 }
-                if(GetAsyncKeyState(VK_RIGHT) & 0x8000) {
+                if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
                     g_me->SetTwCommand(RIGHT);
                 }
-                if(GetAsyncKeyState(VK_UP) & 0x8000) {
+                if (GetAsyncKeyState(VK_UP) & 0x8000) {
                     g_me->SetTwCommand(UP);
                 }
             }
@@ -182,18 +190,18 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     }
 
     // 如果游戏结束
-    if(g_me->IsOver()) {
-        MessageBoxA(NULL, "出师未捷", "吃豆子提示", MB_OK);
+    if (g_me->IsOver()) {
+        PlaySound(LPWSTR(IDR_WAVE1), GetModuleHandle(NULL), SND_RESOURCE | SND_ASYNC | SND_LOOP);
+        MessageBoxA(NULL, "寄", "提示", MB_OK);
     }
     // 否则,提示赢得游戏
     else {
-        MessageBoxA(NULL, "恭喜您赢得了胜利\r\n确定后游戏退出", "吃豆子提示", MB_OK);
+        PlaySound(LPWSTR(IDR_WAVE3), GetModuleHandle(NULL), SND_RESOURCE | SND_ASYNC | SND_LOOP);
+        MessageBoxA(NULL, "恭喜您赢得了胜利\r\n确定后游戏退出", "提示", MB_OK);
     }
 
-    return (int) msg.wParam;
+    return (int)msg.wParam;
 }
-
-
 
 //
 //  函数: MyRegisterClass()
@@ -202,21 +210,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
+    PlaySound(LPWSTR(IDR_WAVE2), GetModuleHandle(NULL), SND_RESOURCE | SND_ASYNC | SND_LOOP);
+
     WNDCLASSEXW wcex;
 
     wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PACMAN));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_PACMAN);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PACMAN));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_PACMAN);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     return RegisterClassExW(&wcex);
 }
@@ -228,7 +238,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 //   注释:
 //
-//        在此函数中，我们在全局变量中保存实例句柄并
+//        在此函数中，我们在全局变量中存储实例句柄并
 //        创建和显示主程序窗口。
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
@@ -237,13 +247,13 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
     // 创建窗口
     HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW
-                              , 0 // 在屏幕中的位置 x
-                              , 0 // 在屏幕中的位置 y
-                              , WLENTH // 宽
-                              , WHIGHT // 高
-                              , nullptr, nullptr, hInstance, nullptr);
+        , 0 // 在屏幕中的位置 x
+        , 0 // 在屏幕中的位置 y
+        , WLENTH // 宽
+        , WHIGHT // 高
+        , nullptr, nullptr, hInstance, nullptr);
 
-    if(!hWnd) {
+    if (!hWnd) {
         return FALSE;
     }
 
@@ -253,67 +263,102 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
 
+    // 设置计时器，每 100 毫秒触发一次
+    SetTimer(hWnd, 1, 100, NULL);
+
     return TRUE;
 }
 
 //
 //  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
-//  目的:    处理主窗口的消息。
+//  目的: 处理主窗口的消息。
 //
 //  WM_COMMAND  - 处理应用程序菜单
 //  WM_PAINT    - 绘制主窗口
+//  WM_TIMER    - 处理计时器消息
 //  WM_DESTROY  - 发送退出消息并返回
-//
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch(message) {
-        case WM_COMMAND: {
-            int wmId = LOWORD(wParam);
-            // 分析菜单选择:
-            switch(wmId) {
-                case IDM_ABOUT:
-                    DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                    break;
-                case IDM_EXIT:
-                    DestroyWindow(hWnd);
-                    break;
-                default:
-                    return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-        case WM_PAINT: {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 在此处添加使用 hdc 的任何绘图代码...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
+    static int seconds = 1800; // 计时器初始值, 1800 次 100 毫秒 -> 180 秒
+
+    switch (message) {
+    case WM_COMMAND: {
+        int wmId = LOWORD(wParam);
+        // 分析菜单选择:
+        switch (wmId) {
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
             break;
         default:
             return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+                   break;
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        // TODO: 在此处添加使用 hdc 的任何绘图代码...
+
+        // 显示计时器信息
+        int displayTime = seconds / 10; // 转换为秒，100ms * 10 = 1s
+        TCHAR buffer[50];
+        _stprintf_s(buffer, _T("Time: %d s"), displayTime);
+        TextOut(hdc, 10, 10, buffer, _tcslen(buffer));
+
+        EndPaint(hWnd, &ps);
+    }
+                 break;
+    case WM_TIMER: {
+        // 每次计时器触发时执行的操作
+        if (wParam == 1) {
+            if (seconds > 0) {
+                seconds--; // 每 100 毫秒触发一次
+                // 重新绘制窗口
+                InvalidateRect(hWnd, NULL, FALSE);
+            }
+            else {
+                // 计时器到达 0，游戏失败
+                KillTimer(hWnd, 1); // 销毁计时器
+                MessageBox(hWnd, _T("时间到！游戏失败！"), _T("游戏结束"), MB_OK);
+                DestroyWindow(hWnd); // 关闭窗口
+            }
+        }
+    }
+                 break;
+    case WM_DESTROY:
+        KillTimer(hWnd, 1); // 销毁计时器
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
 }
 
-// “关于”框的消息处理程序。
+//
+//  函数: About(HWND, UINT, WPARAM, LPARAM)
+//
+//  目的: 处理关于框的消息。
+//
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     UNREFERENCED_PARAMETER(lParam);
-    switch(message) {
-        case WM_INITDIALOG:
-            return (INT_PTR)TRUE;
+    switch (message) {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
 
-        case WM_COMMAND:
-            if(LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
-                EndDialog(hDlg, LOWORD(wParam));
-                return (INT_PTR)TRUE;
-            }
-            break;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
     }
     return (INT_PTR)FALSE;
 }
+
